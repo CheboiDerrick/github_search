29e00d791ffd40e1154be3683fbbe3cf09b4ee09
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-08-26 23:34:58 +0300

    added github request service and added promise to request user daata

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,12 @@
-export class User {
+import { IUser } from "../interfaces/user";
+export class User implements IUser {
+    constructor(public name: string, public email: string, public avatar: string, public public_repos: number, public followers: number, public following: number, public join_date: string) {
+        this.name = name;
+        this.email = email;
+        this.avatar = avatar;
+        this.public_repos = public_repos;
+        this.followers=followers;
+        this.following=following;
+        this.join_date=join_date
+    }
 }
--- src/app/services/github-request.service.ts
+++ src/app/services/github-request.service.ts
@@ -1,18 +1,38 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 import { IUser } from '../interfaces/user';
 import { IRepo } from '../interfaces/repo';
+import { User } from '../classes/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
-  user!:IUser
-  repos:IRepo[]=[]
+  user!: User
+  repos: IRepo[] = []
 
-  constructor(private http:HttpClient) {
-    this.user = new User("","");
-}
+  constructor(private http: HttpClient) {
+    this.user = new User("", "", "", 0, 0, 0, "");
+  }
+  userRequest(){
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get<IUser>(environment.apiUrl).toPromise().then(response => {
+        this.user.name = response.name;
+        this.user.email = response.email;
+        this.user.avatar = response.avatar;
+        this.user.public_repos = response.public_repos;
+        this.user.followers = response.followers;
+        this.user.following = response.following
+        this.user.join_date = response.join_date
+        resolve()
+      },
+        error => {
+          reject(error)
+        })
+    })
+    return promise
+  }
 }
