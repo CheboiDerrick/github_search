07102c770afed84a3e75d9a4fe6bfde77c7d1019
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-09-01 00:21:14 +0300

    add changes to encironment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com/users/CheboiDerrick",
+  client_id : '20a8d5241ac2df67cbc0',
+  client_secret : '046f7d7186d00765ba7c252f65ea58912c4336c8',
 };
